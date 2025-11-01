93f2e6c1f5cf275b4eca45eefea18f31b796b95e
omar helal <omarhelal951@gmail.com>
2025-11-01 13:30:55 +0200

    Add Expense Model with data validation

---
+++ sw_project/sw_project/Models/Expense.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace sw_project.Models
+{
+    public class Expense
+    {
+        public int ID { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive number.")]
+        public string Description { get; set; } = null!;
+        [Required]
+        public decimal Amount { get; set; }
+        [Required]
+        public DateTime Date { get; set; } = DateTime.Now;
+        public string Category { get; set; } = null!;
+
+    }
+}
