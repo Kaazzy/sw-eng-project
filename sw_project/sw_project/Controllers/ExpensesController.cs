70f299b8f9fb068e907ec2adc404c3635b6a80ef
Kazzy <nowyouseemenow8@gmail.com>
2025-11-23 18:05:41 +0200

    Create ExpensesController.cs
    I created the controllers ... to be able to access DB ...etc

---
+++ sw_project/sw_project/Controllers/ExpensesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sw_project.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
