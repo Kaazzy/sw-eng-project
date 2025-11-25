22c82bb29047f97863313a551ef359542daafcc9
seifyoussef-21 <seifyoussef05@gmail.com>
2025-11-25 14:06:24 +0200

    Refactor ExpensesController for async operations
    Updated Index method to use asynchronous database call and added Create methods for handling expense creation.

--- sw_project/sw_project/Controllers/ExpensesController.cs
+++ sw_project/sw_project/Controllers/ExpensesController.cs
@@ -10,10 +10,25 @@ namespace sw_project.Controllers
         {
             _context = context;
         }
-        public IActionResult Index()
+       public async Task<IActionResult> Index()
         {
-            var expenses = _context.Expenses.ToList();
+            var expenses = await _context.Expenses.ToListAsync();
             return View(expenses);
         }
-    }
-}
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Expense expense)
+        {
+            if(ModelState.IsValid)
+            {
+                await _expensesService.Add(expense);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(expense);
+        }
+    
