82541e8766bcbd3f607e6f8f560936f50da944d0
omar helal <omarhelal951@gmail.com>
2025-12-05 23:53:28 +0200

    update Authcontrollers
    update in AuthController in controllers add [hhtpget] and [httppost] to handle view login page and process and check email and passward.

--- sw_project/sw_project/Controllers/AuthController.cs
+++ sw_project/sw_project/Controllers/AuthController.cs
@@ -4,13 +4,15 @@ namespace sw_project.Controllers
 {
     public class AuthController : Controller
     {
-        
+         [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
-        
+
+        [HttpPost]
+
         public IActionResult Login(string email, string password)
         {
             if (email == "admin@example.com" && password == "123456")
