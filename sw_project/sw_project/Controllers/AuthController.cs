903598e3e34a9faca142d163da0a7fd0be2f2071
seifyoussef-21 <seifyoussef05@gmail.com>
2025-12-05 23:25:56 +0200

    Update AuthController.cs

--- sw_project/sw_project/Controllers/AuthController.cs
+++ sw_project/sw_project/Controllers/AuthController.cs
@@ -4,10 +4,21 @@ namespace sw_project.Controllers
 {
     public class AuthController : Controller
     {
-        // GET: /Auth/Login
-        [HttpGet]
+        
         public IActionResult Login()
         {
+            return View();
+        }
+
+        
+        public IActionResult Login(string email, string password)
+        {
+            if (email == "admin@example.com" && password == "123456")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Invalid email or password!";
             return View();
         }
     }
