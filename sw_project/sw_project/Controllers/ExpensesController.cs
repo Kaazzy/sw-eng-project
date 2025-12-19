9b978b401f4dc76cb0a906059385fbae55895df7
omar helal <omarhelal951@gmail.com>
2025-12-19 21:10:18 +0200

    updated expencescontroller
    Added GetChart action to ExpensesController that returns category totals for the signed-in user as JSON.
    
    Ensured it materializes the query correctly even though the service returns a non-generic IQueryable.

--- sw_project/sw_project/Controllers/ExpensesController.cs
+++ sw_project/sw_project/Controllers/ExpensesController.cs
@@ -3,6 +3,7 @@ using sw_project.Models;
 using sw_project.Services.Interfaces;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace sw_project.Controllers
 {
@@ -27,6 +28,15 @@ namespace sw_project.Controllers
             var userId = GetUserId();
             var expenses = await _expensesService.GetAll(userId);
             return View(expenses);
+        }
+
+        // GET: /Expenses/GetChart
+        [HttpGet]
+        public IActionResult GetChart()
+        {
+            var userId = GetUserId();
+            var data = _expensesService.GetChartData(userId).Cast<object>().ToList();
+            return Json(data);
         }
 
         // GET: Expenses/Create
